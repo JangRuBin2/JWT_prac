56cc7890d365adddd553d5a8f0efd24172a8ce20
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-05-24 22:15:29 +0900

    절대경로 지정 및 static()이용해 root-> 하위의 정적폴더 불러오기

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
-// console.log(express)
+import path from "path";
+const root = path.join(import.meta.url, "../../");
+console.log(root)
 const app = express();
-// console.log(app)
 app.get('/', (req, res)=> {
   res.send("Hello World");
 })
+app.use(express.static(root))
 app.listen(8080,()=> {
   console.log("서버 정상적으로 연결");
 })
