8926b4e6183c84d22f8e0c7829798848ed357c90
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-05-24 22:25:46 +0900

    login.html 파일을 못 찾음

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 import express from "express";
 import path from "path";
-const root = path.join(import.meta.url, "../../");
+const root = path.join(import.meta.url, "../");
 console.log(root)
 const app = express();
-app.get('/', (req, res)=> {
-  res.send("Hello World");
-})
 app.use(express.static(root))
+app.get('/', (req, res)=> {
+  // res.send("Hello World");
+  res.sendFile(path.resolve(root, "login.html"));
+})
+// app.get('/login.html', (req, res)=> {
+//   res.send("Hello World");
+// })
+app.post('/login',(req, res)=> {
+  res.send("하이?")
+})
+
 app.listen(8080,()=> {
   console.log("서버 정상적으로 연결");
 })
