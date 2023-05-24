9536b9964de76c4834cd34838462d3fd7c1a8160
JangRuBin2 <wkdfnqls2465@gmail.com>
2023-05-24 22:08:34 +0900

    최초 페이지 처리 및 서버 정상 작동 확인

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
 import express from "express";
-console.log(express)
+// console.log(express)
+const app = express();
+// console.log(app)
+app.get('/', (req, res)=> {
+  res.send("Hello World");
+})
+app.listen(8080,()=> {
+  console.log("서버 정상적으로 연결");
+})
